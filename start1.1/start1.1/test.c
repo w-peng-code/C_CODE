c48e60dc32f96ddb2abe4bceae0213e9eb688696
w-peng-code <3081960024@qq.com>
2023-10-23 19:47:42 +0800

    start1.1

---
+++ start1.1/start1.1/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+	//int a = 1;
+	//scanf("%d", &a);
+	//printf("%d\n", a);
+	//return 0;
+//}
+//int main()
+//{
+//	char arr[6] = { 0 };
+//	scanf("%s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+int main()
+{
+	printf("hellow!\n");
+
+}
